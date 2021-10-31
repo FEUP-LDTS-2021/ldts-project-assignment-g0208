30419bc545fe4144588593cd81a696e63fc223c0
Edgar Lourenco <edgar.elo65@gmail.com>
2021-10-31 18:31:36 +0000

    Added a welcoming message

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,5 @@
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Thank you for using the Hello World App");
     }
 }
