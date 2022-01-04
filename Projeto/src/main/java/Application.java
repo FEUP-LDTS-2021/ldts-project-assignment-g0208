0176a8ea060eaf8771aab9e766b7867849132a46
Miguel <j.miguel.cr12@gmail.com>
2022-01-04 17:24:55 +0000

    added terminal screen

--- Projeto/src/main/java/Application.java
+++ Projeto/src/main/java/Application.java
@@ -1,5 +1,14 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Game game = new Game();
     }
 }
